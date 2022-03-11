c70ad794f05c64917d7fae6ca620969befa1e8e5
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-03-11 09:20:23 -0500

    finish hashmap

--- gettestie/src/test/java/rocks/zipcode/HashMapTest.java
+++ gettestie/src/test/java/rocks/zipcode/HashMapTest.java
@@ -1,4 +1,36 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+
 public class HashMapTest {
+    @Test
+    public void putTest(){
+        //Given
+        HashMap<String, String> s = new HashMap<>();
+        s.put("1","Ark");
+
+        //When
+        String s1="Ark";
+
+        //Then
+        Assert.assertEquals(s1, s.get("1"));
+    }
+    @Test
+    public void removeTest(){
+        //Given
+        HashMap<String, String> s = new HashMap<>();
+        s.put("1","Ark");
+        s.put("2","Ark1");
+
+        //When
+        s.remove("1");
+
+        //Then
+        Assert.assertFalse(s.containsKey("1"));
+        Assert.assertTrue(s.containsKey("2"));
+        Assert.assertEquals(1,s.size());
+    }
 }
