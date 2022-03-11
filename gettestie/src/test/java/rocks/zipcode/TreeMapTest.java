ca89e75f1b00d8d4f06e0d3f936bcd275161a507
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-03-11 15:12:02 -0500

    finished Treemap?

--- gettestie/src/test/java/rocks/zipcode/TreeMapTest.java
+++ gettestie/src/test/java/rocks/zipcode/TreeMapTest.java
@@ -1,4 +1,40 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class TreeMapTest {
+    @Test
+    public void putGetTest(){
+        //Given
+        Map<Integer, String> s = new TreeMap<>();
+
+        //Then
+        s.put(0,"zero");
+        s.put(-1,"negative");
+
+
+        //When
+        Assert.assertEquals("zero",s.get(0));
+        Assert.assertEquals("negative",s.get(-1));
+    }
+    @Test
+    public void replaceAndKeySeTTest(){
+        //Given
+        Map<Integer, String> s = new TreeMap<>();
+        s.put(0,"zero");
+        s.put(1,"one");
+
+        //Then
+        s.replace(1,"two");
+
+        //When
+        Assert.assertEquals(new Integer[] {0,1}, s.keySet().toArray());
+        Assert.assertFalse(s.get(1).equals("one"));
+        Assert.assertTrue(s.size()==2);
+    }
 }
--- gettestie/src/test/java/rocks/zipcode/TreeSetTest.java
+++ gettestie/src/test/java/rocks/zipcode/TreeSetTest.java
@@ -1,4 +1,14 @@
 package rocks.zipcode;
 
+import org.junit.Test;
+
+import java.util.Set;
+import java.util.TreeSet;
+
 public class TreeSetTest {
+    @Test
+    public void addTest(){
+        Set<Integer> s = new TreeSet<>();
+        s.add(1);
+    }
 }
